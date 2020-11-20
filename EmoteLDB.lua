adf207ace97e457071cddb476c232a5a343a54b4
Cilraaz <cilraaz@gmail.com>
2020-11-20 02:51:02 -0500

    Last (hopefully) lib update

--- EmoteLDB.lua
+++ EmoteLDB.lua
@@ -25,7 +25,7 @@ local string = _G.string
 local pairs = _G.pairs
 local gsub = string.gsub
 
-local emoteVer = "v2020.11.20-2"
+local emoteVer = "v2020.11.20.5"
 
 -- Debug setting
 addon.debug = false
